9ab5ad10b0947ef6f7424b0f5182fdbe03caf701
Arthur Briquet <mdpbriar@henallux.be>
2025-01-14 14:11:56 +0100

    feat: generate id if not exists, else use it from filename

--- hello.py
+++ hello.py
@@ -1,41 +1,11 @@
-import genanki
-import pyexcel
+from src.anki_generator import AnkiGenerator
+
 
 def main():
     # print("Hello from anki-generator!")
 
-    records = pyexcel.get_records(file_name="ankki-preparation.ods")
-
-    my_model = genanki.Model(
-        1607392319,
-        'Simple Model',
-        fields=[
-            {'name': 'Question'},
-            {'name': 'Answer'},
-        ],
-        templates=[
-            {
-                'name': 'Card 1',
-                'qfmt': 'Translate from Nederlands to English :<br/>{{Question}}',
-                'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
-            },
-        ])
-
-    my_deck = genanki.Deck(
-        2059400110,
-        'Leren het Nederlands met Bavo')
-
-    for record in records:
-        print(record)
-        my_note = genanki.Note(
-            model=my_model,
-            fields=[record['Nederlands'], record['English']])
-
-        my_deck.add_note(my_note)
-
-    genanki.Package(my_deck).write_to_file('output.apkg')
-
-    print("deck is generated")
+    anki_generator = AnkiGenerator(excel_path="leren_het_nederlands_met_bavo.xls")
+    anki_generator.generate_anki()
 
 
 if __name__ == "__main__":
---
+++ src/anki_generator.py
@@ -0,0 +1,80 @@
+import random
+from pathlib import Path
+
+import genanki
+import pyexcel
+from random import randrange
+
+def is_castable_to_int(input_string):
+    try:
+        # Attempt to convert the string to an integer
+        int(input_string)
+        return True
+    except ValueError:
+        # If a ValueError is raised, the string is not castable to an integer
+        return False
+
+class AnkiGenerator:
+
+    excel_path: str
+    deck: genanki.Deck
+
+    def __init__(self, excel_path: str) -> None:
+        self.excel_path = excel_path
+
+        # get the deck name from file name
+        anki_deck_name = Path(excel_path).stem
+        parts = anki_deck_name.split("-")
+        id = parts[-1]
+        if is_castable_to_int(id) and int(id) in range(1 << 30, 1 << 31):
+        # if 1 << 30 <= id <= 1 << 31:
+            deck_id = int(id)
+        else:
+            deck_id = randrange(1 << 30, 1 << 31)
+
+        print(f"Anki deck name: {anki_deck_name}")
+
+        self.deck = genanki.Deck(deck_id, anki_deck_name)
+
+
+    def _extract_data_from_sheet(self, sheet_name: str):
+
+        sheet = pyexcel.get_sheet(file_name=self.excel_path, sheet_name=sheet_name)
+        question = sheet[0, 1]
+
+        random.seed(sheet_name)
+        model_id = random.randrange(1 << 30, 1 << 31)
+
+        my_model = genanki.Model(
+            model_id,
+            'Simple Model',
+            fields=[
+                {'name': 'Question'},
+                {'name': 'Answer'},
+            ],
+            templates=[
+                {
+                    'name': 'Card 1',
+                    'qfmt': question + " :<br/>{{Question}}",
+                    'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+                },
+            ])
+        records = pyexcel.get_array(file_name=self.excel_path, sheet_name=sheet_name, start_row=3)
+        for record in records:
+            if record[0] == '' or record[1] == '':
+                continue
+            my_note = genanki.Note(
+                model=my_model,
+                fields=[record[0], record[1]])
+
+            self.deck.add_note(my_note)
+
+    def generate_anki(self) -> None:
+        book = pyexcel.get_book(file_name=self.excel_path)
+        sheet_names = book.sheet_names()
+        for sheet_name in sheet_names:
+            self._extract_data_from_sheet(sheet_name)
+
+        genanki.Package(self.deck).write_to_file(f"{self.deck.name}.apkg")
+
+        print(f"The deck has been generated with id {self.deck.deck_id}, keep this ID, next time you generate the deck, input the id to update the package")
